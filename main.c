bb8867f2ce1f5930693643e8f9e0883edfb8b262
ricardo <ricardo.chagas@usp.br>
2015-06-21 15:24:12 -0300

    Adiciona as funções da main A geraçao do arquivo de log ainda nao foi feita pois a maior parte dele deve ser feita no TAD, entao é melhor esperar ele ficar pronto primeiro :) As funções foram compiladas e parcialmente testadas Ainda não estao comentadas, vou fazer isso o mais rapido possivel

--- main.c
+++ main.c
@@ -1,16 +1,253 @@
 #ifndef TEST
-
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <ctype.h>
 #include "misc.h"
 
+/*
+Funcao main apenas pega a opcao do usuario e direciona o programa para a funcao correspondente
+Variaveis:
+	opc- opcao digitada pelo usuario
+	btree- arvore binaria
+*/
 int main(int argc, char *argv[]) {
-	
+	int opc;
+	arvoreb_t *btree;
+	FILE *reg;
+
+	reg = fopen(FILE_REG, "rb");
+	if (reg != NULL) { //arquivo de registros ja existe e deve ser carregado
+		Load_reg(reg, btree);
+		fclose(reg);
+	}
+
+	btree = createArvoreB();
+	opc = 1;
+	while (opc) {
+		clear();
+		main_menu();
+		scanf("%d", &opc);
+		printf("==========================================================\n");
+
+		switch (opc) {
+			case 1:
+				Insere_usuario(btree);
+				break;
+
+			case 2:
+				Remove_usuario(btree);
+				break;
+
+			case 3:
+				Busca_usuario(btree);
+				break;
+
+			case 4:
+				clear();
+				printArvoreB(btree);
+				break;
+
+			case 5:
+				clear();
+				printf("Fechando o programa...\n");
+				freeArvoreB(btree);
+				opc = 0;
+				break;
+
+			default:
+				printf("Opcao invalida, tente novamente\n");
+				opc = 1; //Garante que o usuario podera escolher novamente
+				sleep(2);
+				break;
+		}
+
+	}
 	return 0;
 }
 
+//Funcao que imprime o menu principal
+void main_menu (void) {
+	printf("======================Menu Principal=====================\n");
+	printf("1-Inserir usuario\n");
+	printf("2-Remover usuario\n");
+	printf("3-Pesquisar por ID\n");
+	printf("4-Mostrar Arvore-B\n");
+	printf("5-Fechar o programa\n");
+	printf("Digite a opcao desejada: ");
+}
+
+/*
+Funcao que insere um usuario na btree e no arquivo de registro, se necessario
+*/
+void Insere_usuario (arvoreb_t *btree) {
+	id_type id;
+	offset_t pos;
+	FILE *reg;
+	int tu;
+	char buff;
+	char usr_name[MAX_USR_NM];
+	char buffer[MAX_REG];
+
+	printf("Digite a id do usuario: ");
+	scanf("%d", &id);
+	getc(stdin);
+	if (searchArvoreB(btree, id) >= 0) { //Registro com mesmo nome ja existe
+		printf("Usuario ja existente.\n");
+		sleep(2);
+		return;
+	}
+
+	printf("Digite o nome do usuario: ");
+	scanf("%[^\n]s", usr_name);
+	getc(stdin);
+
+	printf("Digite o tipo do usuario: ");
+	scanf("%d", &tu);
+	printf("\n");
+
+	//Insere no arquivo de registro
+	reg = fopen(FILE_REG, "ab");
+		sprintf(buffer, "%d|%s|%d|", id, usr_name, tu);
+		pos = ftell(reg);
+		buff = strlen(buffer);
+		fwrite(&buff, sizeof(char), 1, reg);
+		fwrite(buffer, sizeof(char), buff, reg);
+	fclose(reg);
+
+	//Insere na arvore binaria
+	insertArvoreB(btree, id, pos);
+	printf("Insercao feita com sucesso\n");
+	sleep(2);
+}
+
+void Remove_usuario (arvoreb_t *btree) {
+	id_type id;
+	offset_t pos;
+	FILE *reg;
+	char buff; 
+	char buffer[MAX_REG];
+
+	printf("Digite a id do usuario que deseja consultar: ");
+	scanf("%d", &id);
+	printf("\n");
+	pos = searchArvoreB(btree, id);
+	if (pos < 0) { //O usuario nao foi encontrado na arvore-b
+		printf("Usuario nao cadastrado\n");
+		sleep(2);
+		return;
+	}
+
+	//Operação para remoção do registro no arquivo
+	reg = fopen(FILE_REG, "r+b");
+		fseek(reg, SEEK_SET, pos);
+		fread(&buff, sizeof(char), 1, reg);
+		fread(buffer, sizeof(char), buff, reg);
+		altera_srt(buffer);
+		fseek(reg, SEEK_SET, pos+1); //Ja posiciona o ponteiro apos o indicador de tamanho
+		fwrite(buffer, sizeof(char), buff, reg);
+	fclose(reg);
+
+	//Remove da arvore-b
+	removeArvoreB(btree, id);
+	printf("Remocao realizada com sucesso\n");
+	sleep(2);
+}
+
+void Busca_usuario(arvoreb_t *btree) {
+	offset_t pos;
+	usr_t usr;
+	FILE *reg;
+	char buff;
+	char buffer[MAX_REG];
+
+	printf("Digite a id do usuario que deseja consultar: ");
+	scanf("%d", &usr.id);
+	printf("\n");
+	pos = searchArvoreB(btree, usr.id);
+	if (pos < 0) { //Usuario nao foi encontrado
+		printf("Usuario nao cadastrado\n");
+		sleep(2);
+		return;
+	} else { //Grava as informações do usuario
+		reg = fopen(FILE_REG, "rb");
+		if (reg == NULL) {
+			printf("Nao foi possivel encontrar o arquivo de registros\n");
+			sleep(2);
+			return;
+		}
+			fseek(reg, SEEK_SET, pos);
+			fread(&buff, sizeof(char), 1, reg);
+			fread(buffer, sizeof(char), buff, reg);
+			strTOstruct(buffer, &usr);
+		fclose(reg);
+	}
+}
+
+void Load_reg(FILE *reg, arvoreb_t *btree) {
+	usr_t usr;
+	offset_t pos;
+	char buff;
+	char buffer[MAX_REG];
+
+	rewind(reg);
+	while(!feof(reg)) {
+		pos = ftell(reg);
+		fread(&buff, sizeof(char), 1, reg);
+		fread(buffer, sizeof(char), buff, reg);
+		strTOstruct(buffer, &usr);
+
+		//Insere apenas na arvore binaria
+		insertArvoreB(btree, usr.id, pos);
+	}
+}
+
+int altera_srt(char *str) {
+	int i;
+
+	i = 0;
+	while (str[i] != '\0' && str[i] != '|') {
+		i++;
+	}
+	if (str[i] == '|') {
+		str[i+1] = '*'; //Indicador de que o registro foi removido
+		return 0;
+	} else { //O registro esta gravado incorretamente
+		printf("Registro corrompido\n");
+		return -3;
+	}
+}
+
+void strTOstruct(char *str, usr_t *usr) {
+	int i, j;
+	char buffer[10];
+
+	//Para id
+	i = 0;
+	j = 0;
+	while (str[i] != '\0' && str[i] != '|') {
+		buffer[j] = buffer[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+	usr->id = atoi(buffer);
+
+	//Para o nome
+	i++;
+	j = 0;
+	while (str[i] != '\0' && str[i] != '|') {
+		usr->nome[j] = str[i];
+		i++;
+		j++;
+	}
+	usr->nome[j] = '\0';
+
+	//Para o tipo de usuario
+	i++;
+	usr->tu = atoi(str+i);
+}
 #endif // TEST
---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef TEST
+#ifdef __WIN32
+	#include <Windows.h>
+	#define Sleep(time) void sleep(time) 
+#else
+	#include <unistd.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "arvoreB.h"
+#include "misc.h"
+
+#define MAX_USR_NM 50
+#define MAX_REG 70
+#define FILE_REG "registro.reg"
+
+typedef struct REGISTRO {
+	int id;
+	char nome[MAX_USR_NM];
+	int tu;
+} usr_t;
+
+void main_menu(void);
+void Insere_usuario (arvoreb_t *btree);
+void Remove_usuario (arvoreb_t *btree);
+void Busca_usuario(arvoreb_t *btree);
+void Load_reg(FILE *reg, arvoreb_t *btree);
+int altera_srt(char *str);
+void strTOstruct(char *str, usr_t *usr);
+#endif // TEST
