8a932c24f4a5f26901a23f6f2b41162a738258c7
leobastiani <leogbastiani@gmail.com>
2015-07-05 17:46:03 -0300

    versão final

--- test.c
+++ test.c
@@ -18,42 +18,24 @@ Nomes (N°USP):
 #include "arvoreB.h"
 
 int main(int argc, char *argv[]) {
+	freopen("saida.txt", "w", stdout);
+	remove(FILENAMEARVOREB);
+	system("cp arvoreB2.btree arvoreB.btree");
 	arvoreb_t *arv = createArvoreB(); printArvoreB(arv);
-	
-	// inserindo muitos elementos
-	insertArvoreB(arv,  1, 23);         printArvoreB(arv);
-	insertArvoreB(arv,  2, 24);         printArvoreB(arv);
-	insertArvoreB(arv,  3, 34);         printArvoreB(arv);
-	insertArvoreB(arv,  4,  7);         printArvoreB(arv);
-	insertArvoreB(arv,  5, 34);         printArvoreB(arv);
-	insertArvoreB(arv,  6, 16);         printArvoreB(arv);
-	insertArvoreB(arv,  7, 42);         printArvoreB(arv);
-	insertArvoreB(arv,  8, 36);         printArvoreB(arv);
-	insertArvoreB(arv,  9,  1);         printArvoreB(arv);
-	insertArvoreB(arv, 10, 41);         printArvoreB(arv);
-	insertArvoreB(arv, 11, 20);         printArvoreB(arv);
-	insertArvoreB(arv, 12, 23);         printArvoreB(arv);
-	insertArvoreB(arv, 13, 29);         printArvoreB(arv);
-	insertArvoreB(arv, 14, 31);         printArvoreB(arv);
-	insertArvoreB(arv, 15, 29);         printArvoreB(arv);
-
-	// removendo
-	removeArvoreB(arv,  1);             printArvoreB(arv);
-	removeArvoreB(arv,  2);             printArvoreB(arv);
-	removeArvoreB(arv,  3);             printArvoreB(arv);
-	removeArvoreB(arv,  4);             printArvoreB(arv);
-	removeArvoreB(arv,  5);             printArvoreB(arv);
-	removeArvoreB(arv,  6);             printArvoreB(arv);
-	removeArvoreB(arv,  7);             printArvoreB(arv);
-	removeArvoreB(arv,  8);             printArvoreB(arv);
-	removeArvoreB(arv,  9);             printArvoreB(arv);
-	removeArvoreB(arv, 10);             printArvoreB(arv);
-	removeArvoreB(arv, 11);             printArvoreB(arv);
-	removeArvoreB(arv, 12);             printArvoreB(arv);
-	removeArvoreB(arv, 13);             printArvoreB(arv);
-	removeArvoreB(arv, 14);             printArvoreB(arv);
-	removeArvoreB(arv, 15);             printArvoreB(arv);
-
+	srand(4);
+	int N = 500;
+	int max = 500;
+	int i;
+	for(i=0; i<N; i++) {
+		printf("Chaves inseridas: %d\n", i);
+		// int insert = rand() % max;
+		// insertArvoreB(arv, insert, 0); printArvoreB(arv);
+		int remov = rand() % max;
+		removeArvoreB(arv, remov);
+	}
+	removeArvoreB(arv, 447);
+	removeArvoreB(arv, 462);
+	printArvoreB(arv);
 	freeArvoreB(arv);
 	return 0;
 }
